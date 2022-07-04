a003294124c88af2bb52b108fa330d5e37f99869
Kelly Mota <kelly.bdmota@gmail.com>
2022-07-04 18:19:30 -0400

    Add project files.

---
+++ Calculator/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Calculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(OperationViewModel model)
+        {
+            switch(model.Type)
+            {
+                case OperationType.Addition:
+                    model.Result = model.Number1 + model.Number2;
+                    break;
+                case OperationType.Subtraction:
+                    model.Result = model.Number1 - model.Number2;
+                    break;
+                case OperationType.Multiplication:
+                    model.Result = model.Number1 * model.Number2;
+                    break;
+                case OperationType.Division:
+                    if (model.Number2 == 0)
+                    {
+                        ModelState.AddModelError("Error", "Division by zero is not possible.");
+                        return View(model);
+                    }
+
+                    model.Result = (double) model.Number1 / model.Number2;
+                    break;
+                default: return View(model);
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Calculator/Models/OperationViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Calculator.Models
+{
+    public class OperationViewModel
+    {
+        [Required(ErrorMessage = "This field is required.")]
+        public OperationType? Type { get; set; }
+
+        [Required(ErrorMessage= "This field is required.")]
+        [Display(Name = "Number 1")]
+        public long? Number1 { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
+        [Display(Name = "Number 2")]
+        public long? Number2 { get; set; }
+        public double? Result { get; set; }
+    }
+
+    public enum OperationType
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    } 
+}
